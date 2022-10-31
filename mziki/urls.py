2da4f8518e63650bbca4b4f26c010e69cc3f67b6
nzumbi <felixnjema@gmail.com>
2022-11-01 00:48:04 +0300

    created new admin and edited urls

--- mziki/urls.py
+++ mziki/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('mzikiapp/', include('mzikiapp.urls')),
+    path('', include('mzikiapp.urls')),
     path('admin/', admin.site.urls),
 ]
