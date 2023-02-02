025456d8284caa7f7c4f5ba05b4c9d79380489ff
nzumbi <felixnjema@gmail.com>
2023-02-02 23:15:06 +0300

    edit venues view

--- mzikiapp/views.py
+++ mzikiapp/views.py
@@ -6,7 +6,29 @@ from django.views import generic
 
 def index(request):
         return render(request, "mzikiapp/home.html")
-
+        
+def venues():
+  # TODO: replace with real venues data.
+  data = []
+  groups = db.session.query(Venue.city, Venue.state).group_by(Venue.city, Venue.state).all()
+  if not groups: 
+    flash('no venues exists') 
+    return render_template('pages/venues.html')
+  for group in groups:
+    venues = db.session.query(Venue).filter_by(city=group[0], state=group[1]).all()
+    venues_list = [] # a list containig all venues for each group
+    for venue in venues:
+      venues_list.append({
+        "id": venue.id,
+        "name": venue.name,
+        "num_upcoming_shows": len(db.session.query(Show).filter(Show.venue_id == venue.id).filter(Show.start_time > datetime.now()).all())
+      })
+    data.append({
+      "city":group[0],
+      "state":group[1],
+      "venues":venues_list,
+    })
+  return render_template('pages/venues.html', areas=data)
 
 def show_venue(venue_id):
   # shows the venue page with the given venue_id
